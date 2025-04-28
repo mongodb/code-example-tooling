11fd390bd3a8bd08f75963a4c6ec3b33bd6b8602
dacharyc <dachary.carey@mongodb.com>
2025-04-28 14:19:17 -0400

    Add an aggregation to check for a specific substring in code examples

--- audit/dodec/src/PerformAggregation.go
+++ audit/dodec/src/PerformAggregation.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"dodec/aggregations"
-	"dodec/types"
 	"dodec/utils"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -13,18 +12,18 @@ import (
 func PerformAggregation(db *mongo.Database, ctx context.Context) {
 	// The aggregations in this project use one of these data structures. Uncomment the corresponding data structure,
 	// or make duplicates with appropriate names as needed
-	//simpleMap := make(map[string]int)
+	simpleMap := make(map[string]int)
 	//codeLengthMap := make(map[string]types.CodeLengthStats)
 	//nestedOneLevelMap := make(map[string]map[string]int)
 	//nestedTwoLevelMap := make(map[string]map[string]map[string]int)
 	//pageIdChangesCountMap := make(map[string][]types.PageIdChangedCounts)
 	//pageIdsWithNodeLangCountMismatch := make(map[string][]string)
-	newAppliedUsageExampleCounter := types.NewAppliedUsageExampleCounter{
-		ProductCounts:      make(map[string]int),
-		SubProductCounts:   make(map[string]int),
-		AggregateCount:     0,
-		PagesInCollections: make(map[string][]types.PageIdNewAppliedUsageExamples),
-	}
+	//newAppliedUsageExampleCounter := types.NewAppliedUsageExampleCounter{
+	//	ProductCounts:      make(map[string]int),
+	//	SubProductCounts:   make(map[string]int),
+	//	AggregateCount:     0,
+	//	PagesInCollections: make(map[string][]types.PageIdNewAppliedUsageExamples),
+	//}
 
 	// If you just need to get data for a single collection, perform the aggregation using the collection name
 	//simpleMap = aggregations.GetLanguageCounts(db, "pymongo", simpleMap, ctx)
@@ -35,10 +34,12 @@ func PerformAggregation(db *mongo.Database, ctx context.Context) {
 	if err != nil {
 		panic(err)
 	}
+	substringToFindInCodeExamples := "defaultauthdb"
 
 	for _, collectionName := range collectionNames {
 		//simpleMap = aggregations.GetCategoryCounts(db, collectionName, simpleMap, ctx)
 		//simpleMap = aggregations.GetLanguageCounts(db, collectionName, simpleMap, ctx)
+		simpleMap = aggregations.GetStringInCodeNodeCounts(db, collectionName, simpleMap, ctx, substringToFindInCodeExamples)
 		//simpleMap = aggregations.GetLangCountsFromNodes(db, collectionName, simpleMap, ctx)
 		//simpleMap = aggregations.GetLangCountFromLangArrayManually(db, collectionName, simpleMap, ctx)
 		//codeLengthMap = aggregations.GetCodeLengths(db, collectionName, codeLengthMap, ctx)
@@ -54,13 +55,13 @@ func PerformAggregation(db *mongo.Database, ctx context.Context) {
 		//pageIdChangesCountMap = aggregations.GetDocsIdsWithRecentActivity(db, collectionName, pageIdChangesCountMap, ctx)
 		//pageIdsWithNodeLangCountMismatch = aggregations.GetPagesWithNodeLangCountMismatch(db, collectionName, pageIdsWithNodeLangCountMismatch, ctx)
 		//pageIdsWithNodeLangCountMismatch = aggregations.FindDocsMissingProduct(db, collectionName, pageIdsWithNodeLangCountMismatch, ctx)
-		newAppliedUsageExampleCounter = aggregations.FindNewAppliedUsageExamples(db, collectionName, newAppliedUsageExampleCounter, ctx)
+		//newAppliedUsageExampleCounter = aggregations.FindNewAppliedUsageExamples(db, collectionName, newAppliedUsageExampleCounter, ctx)
 	}
 
-	//simpleTableLabel := "Collection"
-	//simpleTableColumnNames := []interface{}{"Collection", "Count"}
-	//simpleTableColumnWidths := []int{30, 15}
-	//utils.PrintSimpleCountDataToConsole(simpleMap, simpleTableLabel, simpleTableColumnNames, simpleTableColumnWidths)
+	simpleTableLabel := "Collection"
+	simpleTableColumnNames := []interface{}{"Collection", "Count"}
+	simpleTableColumnWidths := []int{30, 15}
+	utils.PrintSimpleCountDataToConsole(simpleMap, simpleTableLabel, simpleTableColumnNames, simpleTableColumnWidths)
 
 	//nestedOneLevelTableLabel := "Product Language"
 	//nestedOneLevelTableColumnNames := []interface{}{"Language", "Count"}
@@ -76,5 +77,5 @@ func PerformAggregation(db *mongo.Database, ctx context.Context) {
 	//utils.PrintCodeLengthMapToConsole(codeLengthMap)
 	//utils.PrintPageIdChangesCountMap(pageIdChangesCountMap)
 	//utils.PrintPageIdsWithNodeLangCountMismatch(pageIdsWithNodeLangCountMismatch)
-	utils.PrintPageIdNewAppliedUsageExampleCounts(newAppliedUsageExampleCounter)
+	//utils.PrintPageIdNewAppliedUsageExampleCounts(newAppliedUsageExampleCounter)
 }
---
+++ audit/dodec/src/aggregations/GetStringInCodeNodeCounts.go
@@ -0,0 +1,52 @@
+package aggregations
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"log"
+)
+
+// GetStringInCodeNodeCounts returns the int count for the specified string present in code nodes within the collection.
+func GetStringInCodeNodeCounts(db *mongo.Database, collectionName string, stringCountMap map[string]int, ctx context.Context, substring string) map[string]int {
+	collection := db.Collection(collectionName)
+	stringMatchingPipeline := mongo.Pipeline{
+		{{"$match", bson.D{
+			{"_id", bson.D{{"$ne", "summaries"}}},
+			{"nodes", bson.D{{"$ne", nil}}}, // Ensure nodes is not null
+		}}},
+		{{"$unwind", bson.D{{"path", "$nodes"}}}},
+		// Filter to omit nodes that have been removed from a docs page
+		{{"$match", bson.D{
+			{"$or", bson.A{
+				bson.D{{"nodes.is_removed", bson.D{{"$exists", false}}}},
+				bson.D{{"nodes.is_removed", false}},
+			}},
+		}}},
+		{{"$match", bson.D{
+			{"nodes.code", bson.D{
+				{"$exists", true},
+				{"$type", "string"}, // Ensure code is a string
+			}},
+		}}},
+		{{"$match", bson.D{
+			{"nodes.code", bson.D{{"$regex", substring}, {"$options", "i"}}},
+		}}},
+		{{"$count", "matchingCodeExamples"}},
+	}
+	cursor, err := collection.Aggregate(ctx, stringMatchingPipeline)
+	if err != nil {
+		log.Fatalf("Failed to execute aggregation in collection %s: %v", collectionName, err)
+	}
+	defer cursor.Close(ctx)
+	var results []bson.M
+	if err = cursor.All(ctx, &results); err != nil {
+		log.Fatalf("Error reading cursor: %v", err)
+	}
+	if len(results) == 0 {
+		stringCountMap[collectionName] = 0
+	} else {
+		stringCountMap[collectionName] = int(results[0]["matchingCodeExamples"].(int32))
+	}
+	return stringCountMap
+}

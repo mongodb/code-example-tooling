160bfb11449bb1906b42e820d8cdb96b51242ead
dacharyc <dachary.carey@mongodb.com>
2025-05-07 14:29:56 -0400

    Update string matching prefixes, expand the languages where they may apply

--- audit/gdcd/add-code-examples/utils/HasStringMatchPrefix.go
+++ audit/gdcd/add-code-examples/utils/HasStringMatchPrefix.go
@@ -18,10 +18,12 @@ func HasStringMatchPrefix(contents string, langCategory string) (string, bool) {
 	usingPrefix := "using "
 	mongoConnectionStringPrefix := "mongodb://"
 	alternoConnectionStringPrefix := "mongodb+srv://"
+	curlPrefix := "curl "
 
 	// These prefixes relate to command-line commands that *aren't* MongoDB specific, such as other tools, package managers, etc.
 	mkdir := "mkdir "
 	cd := "cd "
+	touch := "touch "
 	docker := "docker "
 	dockerCompose := "docker-compose "
 	brew := "brew "
@@ -43,8 +45,8 @@ func HasStringMatchPrefix(contents string, langCategory string) (string, bool) {
 	choco := "choco "
 
 	syntaxExamplePrefixes := []string{atlasCli, mongosh}
-	usageExamplePrefixes := []string{importPrefix, fromPrefix, namespacePrefix, packagePrefix, usingPrefix, mongoConnectionStringPrefix, alternoConnectionStringPrefix}
-	nonMongoPrefixes := []string{mkdir, cd, docker, dockerCompose, dockerCompose, brew, yum, apt, npm, pip, goRun, node, dotnet, export, sudo, copyPrefix, tar, jq, vi, cmake, syft, choco}
+	usageExamplePrefixes := []string{importPrefix, fromPrefix, namespacePrefix, packagePrefix, usingPrefix, mongoConnectionStringPrefix, alternoConnectionStringPrefix, curlPrefix}
+	nonMongoPrefixes := []string{mkdir, cd, touch, docker, dockerCompose, dockerCompose, brew, yum, apt, npm, pip, goRun, node, dotnet, export, sudo, copyPrefix, tar, jq, vi, cmake, syft, choco}
 
 	if langCategory == common.Shell {
 		for _, prefix := range syntaxExamplePrefixes {
@@ -55,6 +57,11 @@ func HasStringMatchPrefix(contents string, langCategory string) (string, bool) {
 		for _, prefix := range nonMongoPrefixes {
 			if strings.HasPrefix(contents, prefix) {
 				return common.NonMongoCommand, true
+			}
+		}
+		for _, prefix := range usageExamplePrefixes {
+			if strings.HasPrefix(contents, prefix) {
+				return common.UsageExample, true
 			}
 		}
 		return "Uncategorized", false
@@ -76,6 +83,11 @@ func HasStringMatchPrefix(contents string, langCategory string) (string, bool) {
 		}
 		return "Uncategorized", false
 	} else {
+		for _, prefix := range nonMongoPrefixes {
+			if strings.HasPrefix(contents, prefix) {
+				return common.NonMongoCommand, true
+			}
+		}
 		for _, prefix := range usageExamplePrefixes {
 			if strings.HasPrefix(contents, prefix) {
 				return common.UsageExample, true

ee3bdae4be3121448b62ac3cf5a4b738e0e890f0
krollins-mdb <kyle.rollins@mongodb.com>
2025-05-12 11:19:45 -0500

    Reinstall deps and delete package-lock file

--- audit/ask-cal/astro.config.mjs
+++ audit/ask-cal/astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-
-import netlify from '@astrojs/netlify';
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
+// Vite plugins
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    plugins: [
+      nodePolyfills({
+        exclude: [
+          "fs", // Excludes the polyfill for `fs` and `node:fs`.
+        ],
+        globals: {
+          Buffer: true,
+          global: true,
+          process: true,
+        },
+        protocolImports: true,
+      }),
+    ],
+  },
 });

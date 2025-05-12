df8d2a750be6acaea7b7ea1798b09a25d9275e95
dacharyc <dachary.carey@mongodb.com>
2025-05-12 14:43:39 -0400

    Add a function to consolidate collections for Ask Cal

---
+++ audit/common/CALDocsPage.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type CalDocsPage struct {
+	ID                   bson.ObjectID  `bson:"_id"`              // ObjectID instead of string
+	CodeNodesTotal       int            `bson:"code_nodes_total"` // Keep other fields same as `DocsPage`
+	DateAdded            time.Time      `bson:"date_added"`
+	DateLastUpdated      time.Time      `bson:"date_last_updated"`
+	IoCodeBlocksTotal    int            `bson:"io_code_blocks_total"`
+	Languages            LanguagesArray `bson:"languages"`
+	LiteralIncludesTotal int            `bson:"literal_includes_total"`
+	Nodes                *[]CodeNode    `bson:"nodes"`
+	PageURL              string         `bson:"page_url"`
+	ProjectName          string         `bson:"project_name"`
+	Product              string         `bson:"product"`
+	SubProduct           string         `bson:"sub_product,omitempty"`
+	Keywords             []string       `bson:"keywords,omitempty"`
+	DateRemoved          time.Time      `bson:"date_removed,omitempty"`
+	IsRemoved            bool           `bson:"is_removed,omitempty"`
+}

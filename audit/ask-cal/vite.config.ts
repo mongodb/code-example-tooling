923f2d81ecb4a2596db6156cdc2f05ec1bc3c875
krollins-mdb <kyle.rollins@mongodb.com>
2025-05-12 17:14:38 -0500

    - remove Astro in favor of simple Vite + React stack - new base project includes working Leafygreen UI

---
+++ audit/ask-cal/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      exclude: [
+        "fs" as never, // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+});

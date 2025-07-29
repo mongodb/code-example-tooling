e2b1f0cae5c17800e7269981577ea7b8f88b29da
dacharyc <dachary.carey@mongodb.com>
2025-07-29 08:31:42 -0400

    Add missing comment and update context for other comment

--- github-check-releases/RepoDetails.js
+++ github-check-releases/RepoDetails.js
@@ -2,7 +2,7 @@ export class RepoDetails {
     constructor(owner, repo, productName, testSuiteVersion) {
         this.owner = owner; // the GitHub organization or member who owns the repo
         this.repo = repo; // the name of the repo within the organization or member
-        this.productName = productName;
-        this.testSuiteVersion = testSuiteVersion; // the version of the project currently used in the test suite
+        this.productName = productName; // a human-readable name to refer to in the console output and Jira ticket
+        this.testSuiteVersion = testSuiteVersion; // the (GitHub tag) version of the project currently used in the test suite
     }
 }
